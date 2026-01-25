ab1e9f6db6e986c030ea83e0b086832c2379fe69
aia <163338564+aia-a@users.noreply.github.com>
2026-01-25 02:59:46 +0100

    Write a display program

---
+++ ch02/display.c
@@ -0,0 +1,15 @@
+/* Program to display a pattern */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("       *\n");
+    printf("      *\n");
+    printf("     *\n");
+    printf("*   *\n");
+    printf(" * *\n");
+    printf("  *\n");
+
+    return 0;
+}
